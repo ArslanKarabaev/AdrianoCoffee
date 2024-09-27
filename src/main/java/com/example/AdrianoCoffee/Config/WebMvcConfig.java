b6a464d9edc839887c01f660735f3d193556ce22
ArslanKarabaev <arslankarabaev08@gmail.com>
2024-09-27 15:11:39 +0600

    check WebMvcConfig 2

--- src/main/java/com/example/AdrianoCoffee/Config/WebMvcConfig.java
+++ src/main/java/com/example/AdrianoCoffee/Config/WebMvcConfig.java
@@ -11,8 +11,12 @@ public class WebMvcConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
+
 }
