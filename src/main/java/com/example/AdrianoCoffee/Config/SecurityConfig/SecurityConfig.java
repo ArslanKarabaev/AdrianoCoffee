75b9763cab81c3eaa8ab12be1ed0c92103c99795
ArslanKarabaev <arslankarabaev08@gmail.com>
2025-02-02 19:43:16 +0600

    new 2025 v2 cors

--- src/main/java/com/example/AdrianoCoffee/Config/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/example/AdrianoCoffee/Config/SecurityConfig/SecurityConfig.java
@@ -5,13 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import static com.example.AdrianoCoffee.Enum.Role.ADMIN;
 import static com.example.AdrianoCoffee.Enum.Role.MANAGER;
@@ -25,10 +21,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configure(http))
                 .authorizeHttpRequests(auth -> auth
 
                         .requestMatchers(
-                                "/api/v2/auth/**",
                                 "/api/v2/auth/register",
                                 "/api/v2/auth/authentication",
                                 "/v3/api-docs",
@@ -59,17 +55,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-    /*cors(cors -> {  // Встроенная поддержка CORS
-            CorsConfiguration config = new CorsConfiguration();
-            config.setAllowCredentials(true);
-            config.addAllowedOriginPattern("*"); // Разрешаем все источники
-            config.addAllowedHeader("*"); // Разрешаем любые заголовки
-            config.addAllowedMethod("*"); // Разрешаем любые методы
-
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", config); // Применяем ко всем путям
-
-            cors.configurationSource(source);
-        }).*/
 }
